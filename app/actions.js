01efd05ac3057a2ac2070f5976e528591b9fea48
Gartia <alexstrasza1337@gmail.com>
2023-09-07 18:23:51 -0700

    Fix nextjs formdata format

--- app/actions.js
+++ app/actions.js
@@ -2,9 +2,10 @@
 import nodemailer from "nodemailer";
 
 export async function submitContactForm(formData) {
-  const { name, email, phone, message } = formData;
-
-  // Create a transporter object using the default SMTP transport
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const phone = formData.get("phone");
+  const message = formData.get("message");
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
@@ -15,20 +16,13 @@ export async function submitContactForm(formData) {
     },
   });
 
-  // Set up email data with unicode symbols
   const mailOptions = {
     from: process.env.SMTP_USER,
     to: process.env.SMTP_SENDTO,
     subject: "New Contact Form Submission",
     text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\n\nMessage:\n${message}`,
   };
-  console.log(
-    process.env.SMTP_HOST,
-    process.env.SMTP_PORT,
-    process.env.SMTP_SECURE
-  );
 
-  // Send the email
   try {
     await transporter.sendMail(mailOptions);
     return { message: "Success!" };
