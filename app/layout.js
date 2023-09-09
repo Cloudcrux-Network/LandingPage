d5e1765576103355bab4de3d5f7328494609bb0a
Gartia <alexstrasza1337@gmail.com>
2023-09-09 01:42:15 -0700

    Fix favicon and meta data

--- app/layout.js
+++ app/layout.js
@@ -4,21 +4,30 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 const description =
   "CloudCrux offers many of the services your company may need. Web Hosting, Email, Custom Cloud Solutions and more. Starting at prices of as low as $3.";
+const url = new URL("https://cloudcrux.com/");
+const title = "CloudCrux Network";
+const image = "https://cloudcrux.com/fbpreview.png";
 export const metadata = {
-  metadataBase: new URL("https://cloudcrux.com/"),
+  metadataBase: url,
   author: "Michael Nichols",
   keyword:
     "Cloud,Crux,Network,CloudCrux,Programming,Hosting,CloudCrux Network,cloud service,cloud services,cloud solutions,hosting services,web host, web hosting",
 
-  title: "CloudCrux Network",
-  description: description,
-
+  title,
+  description,
+  twitter: {
+    card: "summary_large_image",
+    url,
+    title,
+    description,
+    image,
+  },
   openGraph: {
-    title: "CloudCrux Network",
-    url: "https://cloudcrux.com/",
+    title,
+    url,
     type: "website",
-    description: description,
-    image: "https://cloudcrux.com/fbpreview.png",
+    description,
+    image,
   },
 };
 
